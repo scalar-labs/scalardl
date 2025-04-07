5f819af2e37528dd32de30d7d6ea87634a16d794
Jun Nemoto <jun.nemoto@gmail.com>
2025-04-07 13:57:07 +0900

    Add generic contract for select functionality

---
+++ generic-contracts/src/main/java/com/scalar/dl/genericcontracts/table/v1_0_0/Scan.java
@@ -0,0 +1,310 @@
+package com.scalar.dl.genericcontracts.table.v1_0_0;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimaps;
+import com.scalar.dl.ledger.contract.JacksonBasedContract;
+import com.scalar.dl.ledger.database.AssetFilter;
+import com.scalar.dl.ledger.exception.ContractContextException;
+import com.scalar.dl.ledger.statemachine.Asset;
+import com.scalar.dl.ledger.statemachine.Ledger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+
+public class Scan extends JacksonBasedContract {
+
+  @Nullable
+  @Override
+  public JsonNode invoke(
+      Ledger<JsonNode> ledger, JsonNode arguments, @Nullable JsonNode properties) {
+
+    // Get the table information
+    String tableName = arguments.get(Constants.QUERY_TABLE).asText();
+    String tableAssetId = getAssetIdForTable(tableName);
+    JsonNode table =
+        ledger
+            .get(tableAssetId)
+            .orElseThrow(() -> new ContractContextException(Constants.TABLE_NOT_EXIST))
+            .data();
+
+    // Scan records
+    ListMultimap<String, JsonNode> conditionsMap =
+        Multimaps.index(
+            arguments.get(Constants.QUERY_CONDITIONS),
+            condition -> condition.get(Constants.CONDITION_COLUMN).textValue());
+    if (isGetAvailable(table, conditionsMap)) {
+      return get(ledger, table, conditionsMap);
+    } else if (isIndexScanAvailable(table, conditionsMap)) {
+      return scan(ledger, table, conditionsMap);
+    } else {
+      throw new ContractContextException(Constants.INVALID_KEY_SPECIFICATION);
+    }
+  }
+
+  private boolean isGetAvailable(JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    String key = table.get(Constants.TABLE_KEY).textValue();
+    String keyType = table.get(Constants.TABLE_KEY_TYPE).textValue();
+    return conditionsMap.containsKey(key)
+        && conditionsMap.get(key).stream()
+            .anyMatch(condition -> isPrimaryKeyCondition(condition, keyType));
+  }
+
+  private boolean isIndexScanAvailable(
+      JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    for (JsonNode index : table.get(Constants.TABLE_INDEXES)) {
+      String indexKey = index.get(Constants.INDEX_KEY).asText();
+      String indexKeyType = index.get(Constants.INDEX_KEY_TYPE).asText();
+      if (conditionsMap.containsKey(indexKey)
+          && conditionsMap.get(indexKey).stream()
+              .anyMatch(condition -> isIndexKeyCondition(condition, indexKeyType))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isPrimaryKeyCondition(JsonNode condition, String keyType) {
+    if (!condition.get(Constants.CONDITION_VALUE).getNodeType().name().equalsIgnoreCase(keyType)) {
+      throw new ContractContextException(Constants.INVALID_KEY_TYPE);
+    }
+
+    return condition
+        .get(Constants.CONDITION_OPERATOR)
+        .textValue()
+        .equalsIgnoreCase(Constants.OPERATOR_EQ);
+  }
+
+  private boolean isIndexKeyCondition(JsonNode condition, String keyType) {
+    String operator = condition.get(Constants.CONDITION_OPERATOR).textValue().toUpperCase();
+    if (operator.equals(Constants.OPERATOR_IS_NULL)) {
+      return true;
+    }
+
+    if (!condition.get(Constants.CONDITION_VALUE).getNodeType().name().equalsIgnoreCase(keyType)) {
+      throw new ContractContextException(Constants.INVALID_INDEX_KEY_TYPE);
+    }
+
+    return operator.equals(Constants.OPERATOR_EQ);
+  }
+
+  private JsonNode getKeyColumnCondition(
+      JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    String keyColumnName = table.get(Constants.TABLE_KEY).textValue();
+    String keyColumnType = table.get(Constants.TABLE_KEY_TYPE).textValue();
+    for (JsonNode condition : conditionsMap.get(keyColumnName)) {
+      if (isPrimaryKeyCondition(condition, keyColumnType)) {
+        return condition;
+      }
+    }
+
+    throw new ContractContextException(Constants.INVALID_KEY_SPECIFICATION);
+  }
+
+  private JsonNode getIndexColumnCondition(
+      JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    for (JsonNode index : table.get(Constants.TABLE_INDEXES)) {
+      String indexKey = index.get(Constants.INDEX_KEY).asText();
+      String indexKeyType = index.get(Constants.INDEX_KEY_TYPE).asText();
+      for (JsonNode condition : conditionsMap.get(indexKey)) {
+        if (isIndexKeyCondition(condition, indexKeyType)) {
+          return condition;
+        }
+      }
+    }
+
+    throw new ContractContextException(Constants.INVALID_KEY_SPECIFICATION);
+  }
+
+  private List<String> getRecordAssetIdsFromIndex(
+      Ledger<JsonNode> ledger, String tableName, String indexAssetId, String keyColumnName) {
+    List<Asset<JsonNode>> indexEntries = ledger.scan(new AssetFilter(indexAssetId));
+    List<String> assetIds = new ArrayList<>();
+
+    for (Asset<JsonNode> indexEntry : indexEntries) {
+      if (!indexEntry.data().has(keyColumnName)) {
+        throw new ContractContextException(Constants.ILLEGAL_INDEX_STATE);
+      }
+      assetIds.add(
+          getAssetIdForRecord(
+              tableName, keyColumnName, indexEntry.data().get(keyColumnName).asText()));
+    }
+
+    return assetIds;
+  }
+
+  private ArrayNode get(
+      Ledger<JsonNode> ledger, JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    String tableName = table.get(Constants.TABLE_NAME).textValue();
+    JsonNode condition = getKeyColumnCondition(table, conditionsMap);
+    String assetId =
+        getAssetIdForRecord(
+            tableName,
+            condition.get(Constants.CONDITION_COLUMN).textValue(),
+            condition.get(Constants.CONDITION_VALUE).asText());
+    ArrayNode results = getObjectMapper().createArrayNode();
+
+    ledger.get(assetId).ifPresent(asset -> results.add(asset.data()));
+
+    return filter(
+        results,
+        conditionsMap.values().stream()
+            .filter(c -> !c.equals(condition))
+            .collect(Collectors.toList()));
+  }
+
+  private ArrayNode scan(
+      Ledger<JsonNode> ledger, JsonNode table, ListMultimap<String, JsonNode> conditionsMap) {
+    String tableName = table.get(Constants.TABLE_NAME).textValue();
+    String keyColumnName = table.get(Constants.TABLE_KEY).textValue();
+    JsonNode condition = getIndexColumnCondition(table, conditionsMap);
+    String indexAssetId = getAssetIdForIndex(tableName, condition);
+    ArrayNode results = getObjectMapper().createArrayNode();
+
+    for (String recordAssetId :
+        getRecordAssetIdsFromIndex(ledger, tableName, indexAssetId, keyColumnName)) {
+      Optional<Asset<JsonNode>> asset = ledger.get(recordAssetId);
+      if (!asset.isPresent()) {
+        throw new ContractContextException(Constants.ILLEGAL_INDEX_STATE);
+      }
+      results.add(asset.get().data());
+    }
+
+    return filter(
+        results,
+        conditionsMap.values().stream()
+            .filter(c -> !c.equals(condition))
+            .collect(Collectors.toList()));
+  }
+
+  private ArrayNode filter(ArrayNode records, List<JsonNode> conditions) {
+    ArrayNode results = getObjectMapper().createArrayNode();
+
+    for (JsonNode record : records) {
+      boolean allMatched = true;
+      for (JsonNode condition : conditions) {
+        if (!match(record, condition)) {
+          allMatched = false;
+          break;
+        }
+      }
+      if (allMatched) {
+        results.add(record);
+      }
+    }
+
+    return results;
+  }
+
+  private boolean match(JsonNode record, JsonNode condition) {
+    String column = condition.get(Constants.CONDITION_COLUMN).textValue();
+    String operator = condition.get(Constants.CONDITION_OPERATOR).textValue().toUpperCase();
+    JsonNode value = condition.get(Constants.CONDITION_VALUE);
+
+    if (!operator.equals(Constants.OPERATOR_IS_NULL)
+        && !operator.equals(Constants.OPERATOR_IS_NOT_NULL)
+        && (!record.has(column) || !record.get(column).getNodeType().equals(value.getNodeType()))) {
+      return false;
+    }
+
+    switch (operator) {
+      case Constants.OPERATOR_EQ:
+        return isEqual(record.get(column), value);
+      case Constants.OPERATOR_NE:
+        return !isEqual(record.get(column), value);
+      case Constants.OPERATOR_LT:
+        return isLessThan(record.get(column), value);
+      case Constants.OPERATOR_LTE:
+        return !isGreaterThan(record.get(column), value);
+      case Constants.OPERATOR_GT:
+        return isGreaterThan(record.get(column), value);
+      case Constants.OPERATOR_GTE:
+        return !isLessThan(record.get(column), value);
+      case Constants.OPERATOR_IS_NULL:
+        return isNull(record, column);
+      case Constants.OPERATOR_IS_NOT_NULL:
+        return !isNull(record, column);
+      default:
+        throw new ContractContextException(Constants.ILLEGAL_ARGUMENT);
+    }
+  }
+
+  private boolean isEqual(JsonNode leftValue, JsonNode rightValue) {
+    JsonNodeType type = leftValue.getNodeType();
+    if (type == JsonNodeType.STRING) {
+      return leftValue.asText().equals(rightValue.asText());
+    } else if (type == JsonNodeType.NUMBER) {
+      return leftValue.decimalValue().compareTo(rightValue.decimalValue()) == 0;
+    } else {
+      return leftValue.equals(rightValue);
+    }
+  }
+
+  private boolean isLessThan(JsonNode leftValue, JsonNode rightValue) {
+    JsonNodeType type = leftValue.getNodeType();
+    if (type == JsonNodeType.STRING) {
+      return leftValue.asText().compareTo(rightValue.asText()) < 0;
+    } else if (type == JsonNodeType.NUMBER) {
+      return leftValue.decimalValue().compareTo(rightValue.decimalValue()) < 0;
+    } else {
+      throw new ContractContextException(Constants.ILLEGAL_ARGUMENT);
+    }
+  }
+
+  private boolean isGreaterThan(JsonNode leftValue, JsonNode rightValue) {
+    JsonNodeType type = leftValue.getNodeType();
+    if (type == JsonNodeType.STRING) {
+      return leftValue.asText().compareTo(rightValue.asText()) > 0;
+    } else if (type == JsonNodeType.NUMBER) {
+      return leftValue.decimalValue().compareTo(rightValue.decimalValue()) > 0;
+    } else {
+      throw new ContractContextException(Constants.ILLEGAL_ARGUMENT);
+    }
+  }
+
+  private boolean isNull(JsonNode record, String column) {
+    return !record.has(column) || record.get(column).isNull();
+  }
+
+  private String getAssetIdForTable(String tableName) {
+    return Constants.PREFIX_TABLE + tableName;
+  }
+
+  private String getAssetIdForRecord(String tableName, String keyColumnName, String key) {
+    return Constants.PREFIX_RECORD
+        + tableName
+        + Constants.ASSET_ID_SEPARATOR
+        + keyColumnName
+        + Constants.ASSET_ID_SEPARATOR
+        + key;
+  }
+
+  private String getAssetIdForIndex(String tableName, JsonNode condition) {
+    String indexKey = condition.get(Constants.CONDITION_COLUMN).textValue();
+    JsonNode indexValue = condition.get(Constants.CONDITION_VALUE);
+    String operator = condition.get(Constants.CONDITION_OPERATOR).textValue().toUpperCase();
+
+    if (operator.equals(Constants.OPERATOR_IS_NULL)) {
+      return getAssetIdForNullIndex(tableName, indexKey);
+    } else {
+      return getAssetIdForIndex(tableName, indexKey, indexValue.asText());
+    }
+  }
+
+  private String getAssetIdForIndex(String tableName, String indexKey, String value) {
+    return Constants.PREFIX_INDEX
+        + tableName
+        + Constants.ASSET_ID_SEPARATOR
+        + indexKey
+        + Constants.ASSET_ID_SEPARATOR
+        + value;
+  }
+
+  private String getAssetIdForNullIndex(String tableName, String indexKey) {
+    return Constants.PREFIX_INDEX + tableName + Constants.ASSET_ID_SEPARATOR + indexKey;
+  }
+}
---
+++ generic-contracts/src/test/java/com/scalar/dl/genericcontracts/table/v1_0_0/SelectTest.java
@@ -0,0 +1,258 @@
+package com.scalar.dl.genericcontracts.table.v1_0_0;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.scalar.dl.ledger.exception.ContractContextException;
+import com.scalar.dl.ledger.statemachine.Ledger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+public class SelectTest {
+  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final String SOME_TABLE_NAME = "Person";
+  private static final String SOME_PRIMARY_KEY_COLUMN = "GovId";
+  private static final String SOME_PRIMARY_KEY_VALUE = "001";
+  private static final String SOME_INDEX_KEY_COLUMN = "lastName";
+  private static final String SOME_INDEX_KEY_COLUMN_VALUE = "Doe";
+  private static final String SOME_COLUMN_STRING = "firstName";
+  private static final String SOME_COLUMN_STRING_VALUE = "John";
+  private static final String SOME_NON_EXISTING_FIELD = "field";
+  private static final String SOME_INVALID_FIELD = "field";
+  private static final String SOME_INVALID_VALUE = "value";
+  private static final String SOME_INVALID_OPERATOR = "op";
+
+  @Spy private Select select = new Select();
+  @Mock private Ledger<JsonNode> ledger;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    MockitoAnnotations.openMocks(this).close();
+  }
+
+  private JsonNode createQueryArguments(ArrayNode conditions) {
+    return mapper
+        .createObjectNode()
+        .put(Constants.QUERY_TABLE, SOME_TABLE_NAME)
+        .set(Constants.QUERY_CONDITIONS, conditions);
+  }
+
+  @Test
+  public void invoke_CorrectArgumentsGiven_ShouldReturnRecords() {
+    // Arrange
+    ObjectNode argument = mapper.createObjectNode();
+    argument.put(Constants.QUERY_TABLE, SOME_TABLE_NAME);
+    argument.set(
+        Constants.QUERY_CONDITIONS,
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_INDEX_KEY_COLUMN)
+                    .put(Constants.CONDITION_VALUE, SOME_INDEX_KEY_COLUMN_VALUE)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_EQ)));
+    argument.set(
+        Constants.QUERY_PROJECTIONS,
+        mapper
+            .createArrayNode()
+            .add(SOME_INDEX_KEY_COLUMN)
+            .add(SOME_COLUMN_STRING)
+            .add(SOME_NON_EXISTING_FIELD));
+    JsonNode records =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(SOME_PRIMARY_KEY_COLUMN, SOME_PRIMARY_KEY_VALUE)
+                    .put(SOME_INDEX_KEY_COLUMN, SOME_INDEX_KEY_COLUMN_VALUE)
+                    .put(SOME_COLUMN_STRING, SOME_COLUMN_STRING_VALUE));
+    JsonNode expected =
+        mapper
+            .createObjectNode()
+            .put(SOME_INDEX_KEY_COLUMN, SOME_INDEX_KEY_COLUMN_VALUE)
+            .put(SOME_COLUMN_STRING, SOME_COLUMN_STRING_VALUE);
+    doReturn(records).when(select).invokeSubContract(Constants.CONTRACT_SCAN, ledger, argument);
+
+    // Act
+    JsonNode actual = select.invoke(ledger, argument, null);
+
+    // Assert
+    assertThat(actual).isNotNull();
+    assertThat(actual.isArray()).isTrue();
+    assertThat(actual.size()).isEqualTo(1);
+    assertThat(actual.get(0)).isEqualTo(expected);
+  }
+
+  @Test
+  public void invoke_InvalidArgumentsGiven_ShouldThrowException() {
+    // Arrange
+    JsonNode argument1 = mapper.createObjectNode().put(Constants.QUERY_TABLE, SOME_TABLE_NAME);
+    JsonNode argument2 =
+        mapper
+            .createObjectNode()
+            .put(SOME_INVALID_FIELD, SOME_INVALID_VALUE)
+            .put(Constants.QUERY_CONDITIONS, SOME_TABLE_NAME);
+    JsonNode argument3 =
+        mapper
+            .createObjectNode()
+            .put(Constants.QUERY_TABLE, 0)
+            .put(Constants.QUERY_CONDITIONS, SOME_TABLE_NAME);
+    JsonNode argument4 =
+        mapper
+            .createObjectNode()
+            .put(Constants.QUERY_TABLE, SOME_TABLE_NAME)
+            .put(SOME_INVALID_FIELD, SOME_INVALID_VALUE);
+    JsonNode argument5 =
+        mapper
+            .createObjectNode()
+            .put(Constants.QUERY_TABLE, SOME_TABLE_NAME)
+            .put(Constants.QUERY_CONDITIONS, SOME_TABLE_NAME);
+
+    // Act Assert
+    assertThatThrownBy(() -> select.invoke(ledger, argument1, null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_QUERY_FORMAT);
+    assertThatThrownBy(() -> select.invoke(ledger, argument2, null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_QUERY_FORMAT);
+    assertThatThrownBy(() -> select.invoke(ledger, argument3, null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_QUERY_FORMAT);
+    assertThatThrownBy(() -> select.invoke(ledger, argument4, null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_QUERY_FORMAT);
+    assertThatThrownBy(() -> select.invoke(ledger, argument5, null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_QUERY_FORMAT);
+    verify(ledger, never()).get(anyString());
+  }
+
+  @Test
+  public void invoke_InvalidArgumentsWithInvalidConditions_ShouldThrowException() {
+    // Arrange
+    ArrayNode conditions1 = mapper.createArrayNode().add(0);
+    ArrayNode conditions2 = mapper.createArrayNode().add(mapper.createObjectNode());
+    ArrayNode conditions3 =
+        mapper.createArrayNode().add(mapper.createObjectNode().put(Constants.CONDITION_COLUMN, 0));
+    ArrayNode conditions4 =
+        mapper
+            .createArrayNode()
+            .add(mapper.createObjectNode().put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING));
+    ArrayNode conditions5 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_OPERATOR, 0));
+    ArrayNode conditions6 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_OPERATOR, SOME_INVALID_OPERATOR));
+    ArrayNode conditions7 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_IS_NULL))
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_VALUE, SOME_COLUMN_STRING_VALUE)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_IS_NOT_NULL));
+    ArrayNode conditions8 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_VALUE, SOME_COLUMN_STRING_VALUE)
+                    .put(SOME_INVALID_FIELD, SOME_INVALID_VALUE)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_EQ));
+    ArrayNode conditions9 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(SOME_INVALID_FIELD, SOME_INVALID_VALUE)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_EQ));
+    ArrayNode conditions10 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_EQ)
+                    .set(Constants.CONDITION_VALUE, null));
+    ArrayNode conditions11 =
+        mapper
+            .createArrayNode()
+            .add(
+                mapper
+                    .createObjectNode()
+                    .put(Constants.CONDITION_COLUMN, SOME_COLUMN_STRING)
+                    .put(Constants.CONDITION_VALUE, true)
+                    .put(Constants.CONDITION_OPERATOR, Constants.OPERATOR_GTE));
+
+    // Act Assert
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions1), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions1.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions2), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions2.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions3), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions3.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions4), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions4.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions5), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions5.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions6), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_OPERATOR + conditions6.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions7), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions7.get(1));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions8), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions8.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions9), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions9.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions10), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_CONDITION_FORMAT + conditions10.get(0));
+    assertThatThrownBy(() -> select.invoke(ledger, createQueryArguments(conditions11), null))
+        .isExactlyInstanceOf(ContractContextException.class)
+        .hasMessage(Constants.INVALID_OPERATOR + conditions11.get(0));
+    verify(ledger, never()).get(anyString());
+  }
+}
