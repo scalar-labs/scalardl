3ffc22ac533cef3486bfe8d22367a4dd3d942962
Jun Nemoto <35618893+jnmt@users.noreply.github.com>
2025-04-17 14:53:16 +0900

    Add generic contract for select functionality (#119)

--- common/src/main/java/com/scalar/dl/genericcontracts/table/v1_0_0/Constants.java
+++ common/src/main/java/com/scalar/dl/genericcontracts/table/v1_0_0/Constants.java
@@ -2,6 +2,12 @@ package com.scalar.dl.genericcontracts.table.v1_0_0;
 
 public class Constants {
 
+  // Metadata
+  public static final String PACKAGE = "table";
+  public static final String VERSION = "v1_0_0";
+  public static final String CONTRACT_SCAN = PACKAGE + "." + VERSION + ".Scan";
+
+  // Constants
   public static final String PREFIX_TABLE = "tbl_";
   public static final String PREFIX_INDEX = "idx_";
   public static final String PREFIX_RECORD = "rec_";
@@ -15,6 +21,20 @@ public class Constants {
   public static final String INDEX_KEY_TYPE = "type";
   public static final String RECORD_TABLE = "table";
   public static final String RECORD_VALUES = "values";
+  public static final String QUERY_TABLE = "table";
+  public static final String QUERY_CONDITIONS = "conditions";
+  public static final String QUERY_PROJECTIONS = "projections";
+  public static final String CONDITION_COLUMN = "column";
+  public static final String CONDITION_VALUE = "value";
+  public static final String CONDITION_OPERATOR = "operator";
+  public static final String OPERATOR_EQ = "EQ";
+  public static final String OPERATOR_NE = "NE";
+  public static final String OPERATOR_LT = "LT";
+  public static final String OPERATOR_LTE = "LTE";
+  public static final String OPERATOR_GT = "GT";
+  public static final String OPERATOR_GTE = "GTE";
+  public static final String OPERATOR_IS_NULL = "IS_NULL";
+  public static final String OPERATOR_IS_NOT_NULL = "IS_NOT_NULL";
   public static final String ASSET_AGE = "age";
 
   // Error messages
@@ -23,10 +43,22 @@ public class Constants {
       "The specified format of the indexes is invalid.";
   public static final String INVALID_RECORD_FORMAT =
       "The specified format of the record is invalid.";
+  public static final String INVALID_QUERY_FORMAT = "The specified format of the query is invalid.";
+  public static final String INVALID_CONDITION_FORMAT =
+      "The specified format of the condition is invalid. Condition: ";
+  public static final String INVALID_PROJECTION_FORMAT =
+      "The specified format of the projection is invalid. Projection: ";
   public static final String INVALID_KEY_TYPE = "The specified key type is invalid.";
   public static final String INVALID_INDEX_KEY_TYPE = "The specified index key type is invalid.";
+  public static final String INVALID_OPERATOR = "The specified operator is invalid. Condition: ";
+  public static final String INVALID_KEY_SPECIFICATION =
+      "At least a condition for the primary key or index key must be properly specified.";
   public static final String TABLE_ALREADY_EXISTS = "The specified table already exists.";
   public static final String TABLE_NOT_EXIST = "The specified table does not exist.";
   public static final String RECORD_KEY_NOT_EXIST = "The record values must have the key.";
   public static final String RECORD_ALREADY_EXISTS = "The specified record already exists.";
+
+  // Internal error messages due to a bug or tampering
+  public static final String ILLEGAL_INDEX_STATE = "The state of the index is illegal.";
+  public static final String ILLEGAL_ARGUMENT = "The specified argument is illegal.";
 }
