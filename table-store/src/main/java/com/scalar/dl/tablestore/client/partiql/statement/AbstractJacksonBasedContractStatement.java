45d41e4f9e514a9760dc18cf64e1eed1b3a783f0
Jun Nemoto <35618893+jnmt@users.noreply.github.com>
2025-09-10 11:51:22 +0900

    Support native json with back quotes (#246)

--- table-store/src/main/java/com/scalar/dl/tablestore/client/partiql/statement/AbstractJacksonBasedContractStatement.java
+++ table-store/src/main/java/com/scalar/dl/tablestore/client/partiql/statement/AbstractJacksonBasedContractStatement.java
@@ -1,9 +1,54 @@
 package com.scalar.dl.tablestore.client.partiql.statement;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.MoreObjects;
 import com.scalar.dl.ledger.util.JacksonSerDe;
+import java.util.Objects;
 
 public abstract class AbstractJacksonBasedContractStatement implements ContractStatement {
 
+  private final String contractId;
+  private final JsonNode arguments;
   protected static final JacksonSerDe jacksonSerDe = new JacksonSerDe(new ObjectMapper());
+
+  public AbstractJacksonBasedContractStatement(String contractId, JsonNode arguments) {
+    this.contractId = contractId;
+    this.arguments = Objects.requireNonNull(arguments);
+  }
+
+  @Override
+  public String getContractId() {
+    return contractId;
+  }
+
+  @Override
+  public String getArguments() {
+    return jacksonSerDe.serialize(arguments);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("contractId", getContractId())
+        .add("arguments", getArguments())
+        .toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof AbstractJacksonBasedContractStatement)) {
+      return false;
+    }
+    AbstractJacksonBasedContractStatement that = (AbstractJacksonBasedContractStatement) o;
+    return Objects.equals(contractId, that.contractId) && Objects.equals(arguments, that.arguments);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(contractId, arguments.hashCode());
+  }
 }
--- table-store/src/main/java/com/scalar/dl/tablestore/client/partiql/statement/InsertStatement.java
+++ table-store/src/main/java/com/scalar/dl/tablestore/client/partiql/statement/InsertStatement.java
@@ -2,52 +2,12 @@ package com.scalar.dl.tablestore.client.partiql.statement;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.MoreObjects;
 import com.scalar.dl.genericcontracts.table.v1_0_0.Constants;
-import java.util.Objects;
 
 public class InsertStatement extends AbstractJacksonBasedContractStatement {
 
-  private static final String contractId = Constants.CONTRACT_INSERT;
-  private final JsonNode arguments;
-
   private InsertStatement(JsonNode arguments) {
-    this.arguments = Objects.requireNonNull(arguments);
-  }
-
-  @Override
-  public String getContractId() {
-    return contractId;
-  }
-
-  @Override
-  public String getArguments() {
-    return jacksonSerDe.serialize(arguments);
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-        .add("contractId", getContractId())
-        .add("arguments", getArguments())
-        .toString();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof InsertStatement)) {
-      return false;
-    }
-    InsertStatement that = (InsertStatement) o;
-    return Objects.equals(arguments, that.arguments);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(arguments);
+    super(Constants.CONTRACT_INSERT, arguments);
   }
 
   private static JsonNode buildArguments(String table, JsonNode values) {
