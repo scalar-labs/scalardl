6ace852431752ac8122621bdf9f017a83e499a06
Jun Nemoto <35618893+jnmt@users.noreply.github.com>
2026-02-03 14:21:52 +0900

    Skip auto-registration when using custom ValidateLedger contract ID (#404)

--- client/src/main/java/com/scalar/dl/client/config/ClientConfig.java
+++ client/src/main/java/com/scalar/dl/client/config/ClientConfig.java
@@ -394,6 +394,11 @@ public class ClientConfig {
     return auditorLinearizableValidationContractId;
   }
 
+  public boolean isDefaultAuditorLinearizableValidationContractIdUsed() {
+    return DEFAULT_AUDITOR_LINEARIZABLE_VALIDATION_CONTRACT_ID.equals(
+        auditorLinearizableValidationContractId);
+  }
+
   public GrpcClientConfig getGrpcClientConfig() {
     return grpcClientConfig;
   }
