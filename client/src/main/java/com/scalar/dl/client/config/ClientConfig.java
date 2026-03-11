efcdcb54c05c8c33b8163c55541dea383d666a27
Jun Nemoto <35618893+jnmt@users.noreply.github.com>
2026-03-11 13:42:32 +0900

    Upgrade ValidateLedger version number (#458)

--- client/src/main/java/com/scalar/dl/client/config/ClientConfig.java
+++ client/src/main/java/com/scalar/dl/client/config/ClientConfig.java
@@ -4,7 +4,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.scalar.dl.client.error.ClientError;
-import com.scalar.dl.client.validation.contract.v1_0_0.ValidateLedger;
+import com.scalar.dl.client.validation.contract.v1_1_0.ValidateLedger;
 import com.scalar.dl.ledger.config.AuthenticationMethod;
 import com.scalar.dl.ledger.config.ConfigUtils;
 import com.scalar.dl.ledger.config.GrpcClientConfig;
