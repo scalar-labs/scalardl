b85ec94727267dc8a53bd0255c07b31379d9c3a2
kota2and3kan <47254383+kota2and3kan@users.noreply.github.com>
2026-05-28 20:19:25 +0900

    Build UBI based container image for OpenShift

--- scripts/docker/lint.sh
+++ scripts/docker/lint.sh
@@ -4,8 +4,9 @@ set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
 
 SCRIPT_ROOT="$(cd "$(dirname "$0")"; pwd)"
 
+DOCKERFILE_NAME=$1
 HADOLINT_VERSION="v2.12.0"
 HADOLINT="${HADOLINT:-docker run --rm -i -v "$(pwd):/mnt" -w "/mnt" "hadolint/hadolint:${HADOLINT_VERSION}" hadolint}"
 
-exec ${HADOLINT} ./Dockerfile
+exec ${HADOLINT} ./${DOCKERFILE_NAME:-Dockerfile}
 # vim: ai ts=2 sw=2 et sts=2 ft=sh
